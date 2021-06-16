7dd90460210a0e930bc7fe50af4e27a39d7142d9
Priyansh Shah <shah.priyansh04@gmail.com>
2021-06-16 18:07:07 +0530

    Added animation on "/" route

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { auth } from 'firebase';
 import Link from 'next/link';
 import { Flex, Button, Divider, Icon, Text, Code } from '@chakra-ui/react';
+import { Fade, ScaleFade, Slide, SlideFade } from '@chakra-ui/react';
 import { useAuth } from '../utils/auth';
 
 const Home = () => {
@@ -12,64 +13,69 @@ const Home = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Flex
-        backgroundColor="#ffffff"
-        justifyContent="center"
-        flexDirection="column"
-        borderRadius={10}
-        border="red"
-        justifyContent="center"
-        alignItems="space-around"
-        color="black"
-      >
-        <Flex alignItems="space-around" justifyContent="space-between" m="25px">
-          <Flex ml="2rem" justifyContent="center" alignItems="center">
-            <Text fontWeight="bold" fontSize="4xl" color="black">
-              LOGO
-            </Text>
-          </Flex>
-          <Flex mt="1rem">
-            {/* <Link href="/"> */}
-            <Button
-              variant="ghost"
-              size="lg"
-              backgroundColor="#000000"
-              overflow="scroll"
-              color="#ffffff"
-              opacity={0.98}
-            >
-              LOGIN
-            </Button>
-            {/* </Link> */}
-            <Link href="/user/signup">
+      <ScaleFade in={true} initialScale={1.5}>
+        <Flex
+          backgroundColor="#ffffff"
+          justifyContent="center"
+          flexDirection="column"
+          borderRadius={10}
+          border="red"
+          justifyContent="center"
+          alignItems="space-around"
+          color="black"
+        >
+          <Flex
+            alignItems="space-around"
+            justifyContent="space-between"
+            m="25px"
+          >
+            <Flex ml="2rem" justifyContent="center" alignItems="center">
+              <Text fontWeight="bold" fontSize="4xl" color="black">
+                LOGO
+              </Text>
+            </Flex>
+            <Flex mt="1rem">
+              {/* <Link href="/"> */}
               <Button
-                variant="outline"
+                variant="ghost"
                 size="lg"
                 backgroundColor="#000000"
+                overflow="scroll"
                 color="#ffffff"
-                ml="1rem"
+                opacity={0.98}
               >
-                SIGNUP
+                LOGIN
               </Button>
-            </Link>
+              {/* </Link> */}
+              <Link href="/user/signup">
+                <Button
+                  variant="outline"
+                  size="lg"
+                  backgroundColor="#000000"
+                  color="#ffffff"
+                  ml="1rem"
+                >
+                  SIGNUP
+                </Button>
+              </Link>
+            </Flex>
           </Flex>
+          <Divider borderColor="#000000" opacity={0.41} boxShadow={1} />
+          <Flex alignItems="center" justifyContent="center" mt="6rem">
+            <Text color="#000000">
+              Current user: <Code>{auth?.user ? auth.user.email : 'None'}</Code>
+            </Text>
+            <Button
+              onClick={(e) => {
+                auth.signout();
+              }}
+            >
+              LOGOUT
+            </Button>
+          </Flex>
+          <Flex />
         </Flex>
-        <Divider borderColor="#000000" opacity={0.41} boxShadow={1} />
-        <Flex alignItems="center" justifyContent="center" mt="6rem">
-          <Text color="#000000">
-            Current user: <Code>{auth?.user ? auth.user.email : 'None'}</Code>
-          </Text>
-          <Button
-            onClick={(e) => {
-              auth.signout();
-            }}
-          >
-            LOGOUT
-          </Button>
-        </Flex>
-        <Flex />
-      </Flex>
+      </ScaleFade>
     </div>
   );
 };
