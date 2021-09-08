0577c44ce442d9f1bfd5a4e6e072a1a179f4c3e4
shahpriyansh04 <46381434+shahpriyansh04@users.noreply.github.com>
2021-09-08 21:48:47 +0530

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,12 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/storage';
 var firebaseConfig = {
-  apiKey: 'AIzaSyBp7KqqA_ieVOq_gfi2cKlrf_U-HukSIVY',
-  authDomain: 'blog-7d745.firebaseapp.com',
-  projectId: 'blog-7d745',
-  storageBucket: 'blog-7d745.appspot.com',
-  messagingSenderId: '1078222586075',
-  appId: '1:1078222586075:web:9e00aab1d1fd5de3e74cfe'
+  
 };
 
 if (!firebase.apps.length) {
